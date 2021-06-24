599c3a44d78a16691eaff4e7a2a4c3876f6a2ec7
user name <user email>
2021-06-25 00:00:59 +0530

    Ability to Work with Logging Level in STS

--- src/main/java/com/example/addressbook/AddressBookApplication.java
+++ src/main/java/com/example/addressbook/AddressBookApplication.java
@@ -2,12 +2,18 @@ package com.example.addressbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class AddressBookApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AddressBookApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(AddressBookApplication.class, args);
+		log.info("Address Book app started in {} Environment",context.getEnvironment().getProperty("environment"));
+		log.info("Address Book DB user is {} ",context.getEnvironment().getProperty("spring.datasource.username"));
 	}
 
 }
